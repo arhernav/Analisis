40ad5f5dc573c329a502e90c03ef5eba1cefc8c7
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2022-12-13 01:41:57 -0600

    Practica terminada

--- Practica4/Main.java
+++ Practica4/Main.java
@@ -1,72 +1,65 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.nio.file.*;
+import java.io.InputStreamReader;
+import java.lang.Integer;
+
 /**
  Clase main 
  */
-
 public class Main{
 
+    
+    /**
+     * Metodo para constuir la grafica del documento dado
+     * Suponemos que el documento cumple con las especificaciónes dadas
+     * @return Grafica - Grafica construida con base al documento
+     */
+    public static Grafica constuirGrafica(Path documento){
+	Grafica grafica = new Grafica();
+	String elementosLinea[];
+	try{
+	    BufferedReader reader = Files.newBufferedReader(documento);
+	    String linea;
+	    elementosLinea = reader.readLine().split(",");
+	    for(String s : elementosLinea)grafica.addElemento(s);
+	    while((linea = reader.readLine()) != null){
+		elementosLinea = linea.split(",");
+		grafica.addArista(elementosLinea[0],Integer.parseInt(elementosLinea[2]), elementosLinea[1]);
+	    }    
+	}catch(Exception e){
+	    System.out.println(e.getMessage());
+	}
+	return grafica;
+    }
+
+
+    
     public static void main(String args[]){
-	
-	Grafica graf = new Grafica();
+	if(args.length <= 0){
+	    System.out.println("Dar documento para lectura");
+	    return;
+	}
+	//Lectura del documento y construcción de la grafica
+	Path documento = Paths.get(args[0]).toAbsolutePath();
+	Grafica graficaDocumento = constuirGrafica(documento);
 
-	graf.addElemento("a");
-	graf.addElemento("b");
-	graf.addElemento("c");
-	graf.addElemento("d");
-	graf.addElemento("e");
-	graf.addElemento("f");
-	graf.addElemento("g");
-	graf.addElemento("h");
+	//Aplicamos Krusal a la grafica,  imprimimos ambas graficas y damos los  datos pedidos
 
-	graf.addArista("a", 1, "b");
-	graf.addArista("a", 11, "b");
-	graf.addArista("b", 2, "c");
-	graf.addArista("b", 12, "c");
-	graf.addArista("c", 3, "d");
-	graf.addArista("c", 13, "d");
-	graf.addArista("d", 4, "e");
-	graf.addArista("d", 14, "e");
-	graf.addArista("e", 5, "f");
-	graf.addArista("e", 15, "f");
-	graf.addArista("f", 6, "g");
-	graf.addArista("f", 16, "g");
-	graf.addArista("g", 7, "h");
-	graf.addArista("g", 17, "h");
-	graf.addArista("h", 8, "a");
-	graf.addArista("h", 18, "a");
-	
-       
+	Grafica mst = graficaDocumento.kruskal();
 
-	System.out.println(graf);
+	System.out.println("Grafica dada:");
+	System.out.println(graficaDocumento);
 
-	Grafica mst = graf.kruskal();
-	
-
-	System.out.println();
+	System.out.println("\n");
+	System.out.println("Grafica despues de aplicar Kruskal:");
 	System.out.println(mst);
 
-	System.out.println();
+	System.out.println("\n");
+	System.out.println("Datos solicitados de la practica:");
 	System.out.println(mst.getPesoYAristas());
 	
-	/*
-        	con.print();
-	System.out.println();
-	con.join("a", "b");
-	con.print();
-	System.out.println();
-	con.join("c", "d");
-		con.print();
-	System.out.println();
-	con.join("d", "e");
-		con.print();
-	System.out.println();
-	con.join("b", "d");
-		con.print();
-		System.out.println();*/
 	
-
-
-
-
 	
     }
     
